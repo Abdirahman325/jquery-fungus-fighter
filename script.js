d9182e39af45e70a420d4cd0e34ae19b70d23698
Abdirahman Farah <abdirahmanfarah122@gmail.com>
2022-10-28 08:15:04 -0500

    ihate fungus

--- script.js
+++ script.js
@@ -4,13 +4,23 @@ $(document).ready(onReady);
 // Feel free to make this to what you want!
 // Example:
 // let fungusHP = 100;
+let myAP = 100;
+let fungusHP = 100;
 
 function onReady() {
+    console.log('Ready to Fight!')
     
     // Make sure you check the index.html file! 
     // There are lots of buttons and things ready for you to hook into here!
-    
-    
+     //each of these button adds an attack
+     $('attack-btn').on('click', arcaneSceptre);
+
+
+     function setInterval(() => {
+        
+     }, interval);(){
+        $('attack')
+     }
     // 🧠 Remember
     // - Handle events that ->
     // - Updates state which is ->
